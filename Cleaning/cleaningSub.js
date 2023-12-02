83419f38069b2585f2c6e7b76cb666e265c4fa22
YaswanthBellana <139348041+YaswanthBellana@users.noreply.github.com>
2023-12-02 17:40:23 +0530

    Add files via upload

---
+++ Cleaning/cleaningSub.js
@@ -0,0 +1,14 @@
+const regularCleanBtnEl = document.getElementById("regularCleanBtn");
+const washroomCleanBtnEl = document.getElementById("washroomCleanBtn");
+const dustingCleanBtnEl = document.getElementById("dustingCleanBtn");
+
+
+regularCleanBtnEl.addEventListener('click',function(){
+    window.location.href = "./../CleaningForm/cleaningProfile.html";
+});
+washroomCleanBtnEl.addEventListener('click',function(){
+    window.location.href = "./../CleaningForm/cleaningProfile.html";
+});
+dustingCleanBtnEl.addEventListener('click',function(){
+    window.location.href = "./../CleaningForm/cleaningProfile.html";
+});
